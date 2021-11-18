4e9bc91c5f2b43992f0e16df918bdd55be0023f6
swapfile3 <swajeetg768@gmail.com>
2021-11-18 17:53:17 +0530

    Use Object with commands

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ A Discord bot to have some fun with my friends
 A `config.json` file needs to be in source directory as-
 ```json
 {
-    "BOT_TOKEN": "---------------------------------",
-    "OWNER_ID": "----------------"
+    "botToken": "---------------------------------",
+    "ownerID": "----------------"
 }
 ```
-Where `BOT_TOKEN` is private token of Discord bot and `OWNER_ID` is Discord ID
+Where `botToken` is private token of Discord bot and `ownerID` is Discord ID
 of bot owner, which will be used in `noCuss` array containing IDs of users
-whom bot will refuse to cuss further more this user can use `no-cuss` command to
+whom bot will refuse to cuss, further more this user can use `no-cuss` command to
 add users to `noCuss` array.
 
 Run the script as `$ node index.js`
--- index.js
+++ index.js
@@ -2,113 +2,106 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 
 const client = new Discord.Client();
-client.login(config.BOT_TOKEN);
+client.login(config.botToken);
 
 const prefix = "~";
 
-const ownerId = config.OWNER_ID;
+const ownerId = config.ownerId;
 
 // Insert cuss words here
-const cussWords = [];
+const cussWords = new Set("good", "sexy", "jodd");
 
 // Whom not to cuss
 const noCuss = [ownerId];
-
-client.on("message", (message) => {
-    if(message.author.bot) return;
-
-    if(!message.content.startsWith(prefix)) return;
-
-    let full_command = message.content.slice(prefix.length);
-    let args = full_command.split(" ");
-    let command = args.shift().toLowerCase();
-
-    switch(command) {
-        case "intro":
-            onIntro(message);
-            break;
-        case "ping":
-            onPing(message);
-            break;
-        case "cuss":
-            onCuss(message, args);
-            break;
-        case "no-cuss":
-            onNoCuss(message, args);
-            break;
-        default:
-            message.reply(`${command}: Command not found`)
-    }
-});
-
-const onIntro = (message) => {
-    message.channel.send("Hello everyone! I am cuss-bot, I cuss people on request.");
-}
-
-const onPing = (message) => {
-    let timeTaken = Date.now() - message.createdTimestamp;
-    message.channel.send(`Ping ${timeTaken}ms.`);
-}
-
-const onCuss = (message, args) => {
-    if(args.length == 0) {
-        message.channel.send("Err: command 'cuss' requires an argument");
-        return;
-    }
-    let mention = args[0]
-    let userId = getUserId(mention);
-    if(userId === "") {
-        message.channel.send("Err: User not found");
-        return;
-    }
-    if(noCuss.includes(userId)) {
-        message.reply("Can't cuss this user!");
-        return;
-    }
-    let userName = client.users.cache.get(userId);
-    if(userName == undefined) {
-        message.reply("Can't cuss this user!");
-        return;
-    }
-    let cussIndex = Math.floor(Math.random() * cussWords.length);
-    message.channel.send(`${mention} ${cussWords[cussIndex]}`)
-}
-
-const onNoCuss = (message, args) => {
-    if(message.author.id !== "" + ownerId) {
-        message.reply("Only authorized members can use this command!");
-        return;
-    }
-    if(args.length == 0) {
-        message.channel.send("Err: command 'no-cuss' requires an argument");
-        return;
-    }
-    let mention = args[0];
-    let userId = getUserId(mention);
-    if(userId === "") {
-        message.channel.send("Err: User not found");
-        return;
-    }
-    if(noCuss.includes(userId)) {
-        message.reply("Already in no-cuss members.");
-        return;
-    }
-    noCuss.push(userId);
-    message.reply("Okay! Not cussing that person anymore.");
-    /* TODO: Fix @invalid-user mention on line 104
-    let userName = client.users.cache.get(userId);
-    if(userName == undefined) {
-        message.reply("Okay! Not cussing that person anymore.");
-        return;
-    }
-    message.reply(`Okay! Not cussing ${mention} anymore`);
-    console.log(mention);
-    console.log(args[0]);
-    */
-}
 
 const getUserId = (mention) => {
     let userId = mention.includes('<@!') ? mention.replace('<@!', '').replace('>', '')
         : mention.includes('<@') ? mention.replace('<@', '').replace('>', '') : '';
     return userId
 }
+
+const commands = {
+    "intro": (message) => {
+        message.channel.send("Hello everyone! I am cuss-bot, I cuss people on request.");
+    },
+    "ping": (message) => {
+        let timeTaken = Date.now() - message.createdTimestamp;
+        message.channel.send(`Ping ${timeTaken}ms.`);
+    },
+    "cuss": (message, args) => {
+        if(args.length == 0) {
+            message.channel.send("Err: command 'cuss' requires an argument");
+            return;
+        }
+        let mention = args[0]
+        let userId = getUserId(mention);
+        if(userId === "") {
+            message.channel.send("Err: User not found");
+            return;
+        }
+        if(noCuss.includes(userId)) {
+            message.reply("Can't cuss this user!");
+            return;
+        }
+        let userName = client.users.cache.get(userId);
+        if(userName == undefined) {
+            message.reply("Can't cuss this user!");
+            return;
+        }
+        let cussIndex = Math.floor(Math.random() * cussWords.length);
+        message.channel.send(`${mention} ${cussWords[cussIndex]}`)
+    },
+    "no-cuss": (message, args) => {
+        if(message.author.id !== "" + ownerId) {
+            message.reply("Only authorized members can use this command!");
+            return;
+        }
+        if(args.length == 0) {
+            message.channel.send("Err: command 'no-cuss' requires an argument");
+            return;
+        }
+        let mention = args[0];
+        let userId = getUserId(mention);
+        if(userId === "") {
+            message.channel.send("Err: User not found");
+            return;
+        }
+        if(noCuss.includes(userId)) {
+            message.reply("Already in no-cuss members.");
+            return;
+        }
+        noCuss.push(userId);
+        message.reply("Okay! Not cussing that person anymore.");
+        /* TODO: Fix @invalid-user mention
+        let userName = client.users.cache.get(userId);
+        if(userName == undefined) {
+            message.reply("Okay! Not cussing that person anymore.");
+            return;
+        }
+        message.reply(`Okay! Not cussing ${mention} anymore`);
+        console.log(mention);
+        console.log(args[0]);
+        */
+    },
+    "add-cuss": (message, args) => {
+        if(args.length == 0) {
+            message.channel.send("Err: command 'add-cuss' requires an argument");
+            return;
+        }
+        cussWords.add(args[0]);
+        message.channel.send(`${args[0]} added to cuss words`);
+    }
+}
+
+client.on("message", (message) => {
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    let fullCommand = message.content.slice(prefix.length);
+    let args = fullCommand.split(" ");
+    let command = args.shift().toLowerCase();
+    try {
+        commands[command](message, args);
+    } catch {
+        message.reply(`${command}: Can not execute`)
+    }
+});
